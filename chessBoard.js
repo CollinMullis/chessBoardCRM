dd6ac1c98601e9d9b5c601d6d2edb90cd3eaeb51
Collin Mullis <cmullis@saintjoe.edu>
2015-04-14 00:52:34 -0400

    Added for loops and if statements

---
+++ chessBoard.js
@@ -0,0 +1,14 @@
+/* Code by Collin Mullis
+	Javascript Exercise
+	April 13th 2015
+*/
+var line1 = " " 
+var line2 = "#"
+for (i = 0; i <= 8;i++) {
+	// If line # is odd, print first line
+	if (i % 2 !== 0)
+		console.log(line1)
+	// If line # is even, print second line
+	if (i % 2 == 0)
+		console.log(line2)
+	}
