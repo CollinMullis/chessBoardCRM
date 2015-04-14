731f1b1df202e969c5414814ce5ae94657428bfe
Collin Mullis <cmullis@saintjoe.edu>
2015-04-14 01:08:42 -0400

    Added populater to work with "Size" variable

--- chessBoard.js
+++ chessBoard.js
@@ -2,14 +2,27 @@
 	Javascript Exercise
 	April 13th 2015
 */
-var line1 = " # # # #" 
-var line2 = "# # # # "
-size = 8
-for (i = 1; i <= size;i++) {
+var line1 = "" 
+var line2 = ""
+size = 20 
+for (n = 1; n <= size; n++) {
+	// Populating the variables to work for width. 
+	if (n % 2 !== 0) {
+		line1 += " "
+		line2 += "#"
+	}
+	else if (n % 2 == 0) {
+		line1 += "#"
+		line2 += " ";
+	}
+}	
+for (i = 1; i <= size; i++) {
 	// If line # is odd, print first line
-	if (i % 2 !== 0)
+	if (i % 2 !== 0) {
 		console.log(line1)
+		}
 	// If line # is even, print second line
-	else if (i % 2 == 0)
+	else if (i % 2 == 0) {
 		console.log(line2)
+		}
 	}
